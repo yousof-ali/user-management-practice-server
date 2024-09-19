51e7f2392c64cb5fc25c180a9ceb8f071bad2cb9
yousof-ali <yousofali293@gmail.com>
2024-09-19 18:26:31 +0600

    fixed use

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.lewcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient
